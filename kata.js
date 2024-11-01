22879c58cf95d20d2ef2686dd93a86474841c330
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-11-01 10:51:49 -0400

    Update kata.js

--- kata.js
+++ kata.js
@@ -1,1 +1,6 @@
+/* Rock Paper Scissors (RPS) aka Rochambo
+ * Duplicate this chart: https://lucid.app/lucidchart/23afcdbd-efee-4546-91c6-17122becaaec/view
+ * Paste yours, published:
+ * We are coding from individual charts, so minimal structure below. 
+ */
 
