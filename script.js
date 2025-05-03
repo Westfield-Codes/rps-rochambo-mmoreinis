353e9c2664d937f7b73582b1bb25b16a92afe556
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-05-03 15:15:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+/* Steps 
+*  0. Refer to https://lucid.app/lucidchart/3da5e3b5-33fe-4823-875c-81fb61c9d84c/view
+*  1. Create Documentation and stubs for each function. //
+*  2. Unit Test each function:
+*     a. Add pseudocode based on flowchart, picking version
+*     b. Test with console.log using stubs
+*     c. Commit when it works.
+*     d. Move to next function
+*  3. System Test finished version (does it work right in all conditions?)
+*/
