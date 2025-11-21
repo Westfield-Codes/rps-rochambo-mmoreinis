f1efca034351a90f9f13c535e2b3c8054746b7f6
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-11-21 23:56:21 +0000

    Cleaning

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var score = [0, 0];
 var rounds = 5;
 var round = 1;
 var board = document.getElementById("gameBoard");
-var move = "rock";
 var scoreBoard = document.getElementById("scoreBoard");
+var moveWords = ["rock", "paper", "scissors"];
+var moves = ["r", "p", "s"];
 
 function main() {
-   
    document.getElementById("playButton"). style.display = "none";
    let instructions = document.createElement("p");
    instructions.innerHTML = "How many rounds would you like to play? (1-10)";
@@ -20,65 +20,68 @@ function main() {
    roundsButton.addEventListener("click", setRounds);
    board.appendChild(roundsButton);
 }
-function buildConsole() { 
-   board.innerHTML = "";
-   let playRock = document.createElement("div");
-   playRock = document.createElement("BUTTON");
-   playRock.id="rock";
-   playRock.innerHTML="rock";
-   playRock.className="move";
-   playRock.addEventListener("click",playingRock);
-   board.appendChild(playRock);
-   let playScissors = document.createElement("div");
-   playScissors = document.createElement("BUTTON");
-   playScissors.id="scissors";
-   playScissors.innerHTML="scissors";
-   playScissors.addEventListener("click",playingScissors);
-   playScissors.className="move"
-   board.appendChild(playScissors);
-   let playPaper = document.createElement("div");
-   playPaper = document.createElement("BUTTON");
-   playPaper.id="paper";
-   playPaper.innerHTML="paper";
-   playPaper.addEventListener("click",playingPaper);
-   playPaper.className="move"
-   board.appendChild(playPaper);
-   let lineBreak = document.createElement("br");
-   board.appendChild(lineBreak);
-}
+
 function buildScoreBoard(){
-let roundNumber = document.createElement("p");
+   let roundNumber = document.createElement("p");
    roundNumber.id="roundNumber";
    roundNumber.innerHTML="Round " + round + " of " + rounds;
    scoreBoard.appendChild(roundNumber);
-   let player = document.createElement("div");
-   player.id = "player";
-   player.innerHTML = "Player" + ": " + score[0];
-   scoreBoard.appendChild(player);
-   let computer = document.createElement("div");
-   computer.id = "computer";
-   computer.innerHTML = "Computer"+ ": " + score[1];
-   scoreBoard.appendChild(computer);
-
+   addScoreBox("player", "Player", 0);
+   addScoreBox("computer", "Computer", 1);
 }
+
+function addScoreBox(entity, entityLabel, index){
+   let myDiv = document.createElement("div");
+   myDiv.id = entity;
+   myDiv.innerHTML = entityLabel + ": " + score[index];
+   scoreBoard.appendChild(myDiv);
+}
+
+function buildConsole() { 
+   board.innerHTML = "";
+   addPlayButton("rock", "r");
+   addPlayButton("paper", "p");
+   addPlayButton("scissors","s");
+}
+
+function addPlayButton(hand, move){
+    let playButton = document.createElement("div");
+    playButton = document.createElement("button");
+    playButton.id=hand;
+    playButton.innerHTML=hand;
+    playButton.addEventListener('click', () => {
+      cpuTurn(move); 
+    });
+    playButton.className="move"
+    board.appendChild(playButton);
+}
+
 function setRounds() {
   rounds = parseInt(document.getElementById("roundsBox").value);
   buildConsole();
   buildScoreBoard();
-  
 }
-function playingPaper(){
-   move = "paper";
-   cpuTurn();
+
+function cpuTurn(move) {
+   console.log("Move: " + move);
+   let u = moves[moveWords.indexOf(move)];
+   let turn = Math.floor(Math.random() * 3);
+   c = moves[turn];
+   if(u == c) {
+      console.log("CPUTurn")
+      clearConsole();
+      buildConsole();
+    }
+    else{
+      let combo = u + c;
+      let winner = findWinner(combo);
+      let cmove = moveWords[turn];
+      let RoundWinner = document.createElement("div");
+      RoundWinner.innerHTML= "You chose " + "(" + move + ")" + " and I chose " + "(" + cmove + ")" + " " + winner + " won!";
+      round++;
+   }
 }
-function playingRock(){
-   move = "rock";
-   cpuTurn();
-}
-function playingScissors(){
-   move = "scissors";
-   cpuTurn();
-}
+
 function scoreBoard(winner) {
    if (winner == "I") score[1] += 1;
    else score[0] += 1;
@@ -98,37 +101,17 @@ function userTurn() {
    if (!moves.includes(choice)) {
       alert("Invalid Input!")
       return userTurn();
-   } else {
+   } 
+   else {
       return choice;
    }
 }
+
 function clearConsole(){
    board.innerHTML="";
    board.innerHTML="We both chose the same thing";
 }
 
-function cpuTurn() {
-   
-   
-   let moveWords = ["rock", "paper", "scissors"];
-   let moves = ["r", "p", "s"];
-   let u = moves[moveWords.indexOf(move)];
-   let turn = Math.floor(Math.random() * 3);
-   c = moves[turn];
-   if(u == c) {
-      console.log("CPUTurn")
-      clearConsole();
-      buildConsole();
-    }
-    else{
-      let combo = u + c;
-      let winner = findWinner(combo);
-      let cmove = moveWords[turn];
-      let RoundWinner = document.createElement("div");
-      RoundWinner.innerHTML= "You chose " + "(" + move + ")" + " and I chose " + "(" + cmove + ")" + " " + winner + " won!";
-      round++;
-}
-}
 function findWinner(combo) {
    let match = "";
    let winner = "";
